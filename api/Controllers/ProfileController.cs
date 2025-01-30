79856ed3e92a6bb09f6e70dd3aaf45235aa15b3f
franciscoylderlan <ylderlan@nca.ufma.br>
2025-01-30 12:24:18 -0300

    :adhesive_bandage: fix: resolve vs configs

---
+++ api/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    // localhost:xxxx/api/profile
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProfile()
+        {
+            return Ok(new { response = "Hello world!" });
+        }
+    }
+}
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32d2726b524b779327334f78addf01072c2b4f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344850bb6db3673ff401f7495a3459e69f7e1158")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
