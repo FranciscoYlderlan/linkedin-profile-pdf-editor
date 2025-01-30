ac39430734ccc50e00b7972e0a30502ef004e60c
franciscoylderlan <ylderlan@nca.ufma.br>
2025-01-30 15:23:52 -0300

    :sparkles: feat: create profile DTO

---
+++ api/Models/Entities/ProfileDto.cs
@@ -0,0 +1,40 @@
+﻿namespace api.Models.Entities
+{
+    public class Profile
+    {
+        public string? Name { get; set; }
+        public List<ContactInfo>? ContactInfos { get; set; }
+        public string? About { get; set; }
+        public List<Experience>? Experiences { get; set; }
+        public List<Education>? Educations { get; set; }
+        public List<Certification>? Certifications { get; set; }
+    }
+
+    public class ContactInfo
+    {
+        public string? Header { get; set; }
+        public List<string>? Links { get; set; }
+    }
+
+    public class Experience
+    {
+        public string? Company { get; set; }
+        public string? Duration { get; set; }
+        public string? JobTitle { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class Education
+    {
+        public string? Institution { get; set; }
+        public string? Degree { get; set; }
+    }
+
+    public class Certification
+    {
+        public string? CertificationName { get; set; }
+        public string? Issuer { get; set; }
+        public string? DateIssued { get; set; }
+    }
+
+}
